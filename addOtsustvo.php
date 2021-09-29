2159151b569d3721d25a05a4e610bbd6ff500916
manasievak <43408563+manasievak@users.noreply.github.com>
2021-09-29 18:37:38 +0200

    Add files via upload

--- addOtsustvo.php
+++ addOtsustvo.php
@@ -16,18 +16,25 @@ $o_opis = pg_escape_string($db_conn, trim($request->o_opis));
 //$v_id = pg_escape_string($db_conn, trim($request->v_id));
 //$vid_id = pg_escape_string($db_conn, trim($request->vid_id));
 
+$result = pg_query($db_conn, "SELECT o_id from otsustva as o WHERE o.v_id=$v_id and o.o_datum=to_date('$o_datum','yyyy-mm-dd')");
+//ispituva, ako ima otsustvo na taa data za toj vraboten ne vnesuva nisto vo tabela
+if(pg_num_rows($result)==0)
 
-	$sql = "INSERT INTO otsustva (o_datum, o_casovi, o_opis, v_id, vid_id) 
+{
+  $sql = "INSERT INTO otsustva (o_datum, o_casovi, o_opis, v_id, vid_id) 
 VALUES ('$o_datum', '$o_casovi','$o_opis','$v_id','$vid_id')";
 
-if(pg_query($db_conn, $sql))
-{
-  http_response_code(201);
+	if(pg_query($db_conn, $sql))
+	{
+ 	 http_response_code(201);
+	}
+	else
+	{
+ 	 return http_response_code(422);
+	}
 }
-else
-{
-  return http_response_code(422);
-}
+
+	
 }
 
 ?>
